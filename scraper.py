90e6ab57fd18039a272dae068225904b0947085f
Kai LI <kai_li@outlook.com>
2020-12-08 23:53:38 +0000

    RTX stock checker

--- scraper.py
+++ scraper.py
@@ -10,26 +10,26 @@ class Scraper:
     http = urllib3.PoolManager(headers=httpHeader)
 
     @staticmethod
-    def _fetch_url(url):
+    def fetch_url(url):
         resp = Scraper.http.request('GET', url)
         return BeautifulSoup(resp.data, "html5lib")
 
     @staticmethod
     def get_john_lewis_price(url):
-        soup = Scraper._fetch_url(url)
+        soup = Scraper.fetch_url(url)
         return (Stores.John_Lewis.name,
                 float(soup.find("p",class_="price price--large").text.strip()[1:]))
 
     @staticmethod
     def get_argos_price(url):
-        soup = Scraper._fetch_url(url)
+        soup = Scraper.fetch_url(url)
         return (Stores.Argos.name,
                 float(soup.find("li", {"data-test": "product-price-primary"})
                         .get('content').strip()))
 
     @staticmethod
     def get_currys_price(url):
-        soup = Scraper._fetch_url(url)
+        soup = Scraper.fetch_url(url)
         return (Stores.Currys.name,
                 float(soup.find("strong", {"class": "current",
                                            "data-key":"current-price"})
